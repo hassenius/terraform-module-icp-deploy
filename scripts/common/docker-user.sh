2271840388c56332a70afc81d1083dc73c7a434c
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2018-01-12 09:46:55 +0000

    RHEL fixes

--- scripts/common/docker-user.sh
+++ scripts/common/docker-user.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-iam=$(whoami)
-
-if [[ $iam != "root" ]]
+# Some RHEL based installations may not have docker installed yet.
+# Only aattempt to add user to group if docker is installed and the user is not root
+if grep -q docker /etc/group
 then
-  sudo usermod -a -G docker $iam
+  iam=$(whoami)
+
+  if [[ $iam != "root" ]]
+  then
+    sudo usermod -a -G docker $iam
+  fi
 fi
--- scripts/common/prereqs.sh
+++ scripts/common/prereqs.sh
@@ -45,34 +45,18 @@ crlinux_install(){
 #  hostnamectl set-hostname $HOSTNAME
   #install epel
   cd /tmp
-  wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-  yum -y install local epel-release-latest-7.noarch.rpm
-  yum clean all
-  yum repolist
+  sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E '%{rhel}').noarch.rpm
+  sudo yum clean all
+  sudo yum repolist
   #Install net-tools
-  yum -y install net-tools
-  sysctl -w vm.max_map_count=262144
-  systemctl disable firewalld
-  systemctl stop firewalld
-  yum -y install unzip
-  yum -y install moreutils
-  yum -y install ntp
-  yum -y install python-pip
-  yum -y install python-setuptools
+  sudo yum -y install net-tools
+  sudo sysctl -w vm.max_map_count=262144
+  sudo systemctl disable firewalld
+  sudo systemctl stop firewalld
+  sudo yum -y install unzip moreutils ntp python-pip python-setuptools nfs-utils wget
   #add docker repo and install
-  yum install -y yum-utils device-mapper-persistent-data lvm2
-  yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-  #yum-config-manager --enable docker-ce-edge
-  #yum-config-manager --enable docker-ce-testing
-  #yum-config-manager --disable docker-ce-edge
-  yum -y makecache fast
-  yum -y install docker-ce
-  mkdir /etc/docker
-  echo "{ \"storage-driver\": \"devicemapper\" }" > /etc/docker/daemon.json 
-  systemctl enable docker
-  systemctl start docker
-  pip install docker-py --upgrade pip
-  echo 32000 1024000000  500  32000 > /proc/sys/kernel/sem
+  sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+
 }
 
 if [ "$OSLEVEL" == "ubuntu" ]; then

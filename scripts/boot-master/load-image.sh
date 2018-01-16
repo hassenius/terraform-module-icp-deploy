5d97168f18fa3c02650ac825c764bc67cd54ef41
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2018-01-16 16:26:21 +0000

    fix enterprise image filename name

--- scripts/boot-master/load-image.sh
+++ scripts/boot-master/load-image.sh
@@ -32,7 +32,7 @@ then
   elif [[ "${image_location:0:4}" == "http" ]]
   then
     # Figure out what we should name the file
-    filename="ibm-cloud-private.${tag}.tar.gz"
+    filename="ibm-cloud-private.${tag%-ee}.tar.gz"
     mkdir -p ${sourcedir}
     wget -O ${sourcedir}/${filename} "${image_location#http:}"
     image_file="${sourcedir}/${filename}"

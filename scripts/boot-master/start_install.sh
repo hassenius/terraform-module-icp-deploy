d4fce1854fe6e22caa9bc7c766cee2b5e5085a29
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2018-09-28 17:03:23 +0100

    make sure script exits with the exit code of docker

--- scripts/boot-master/start_install.sh
+++ scripts/boot-master/start_install.sh
@@ -9,3 +9,5 @@ echo "registry=${registry:-not specified} org=$org repo=$repo tag=$tag"
 verbosity=${2}
 
 docker run -e LICENSE=accept -e ANSIBLE_CALLBACK_WHITELIST=profile_tasks,timer --net=host -t -v /opt/ibm/cluster:/installer/cluster ${registry}${registry:+/}${org}/${repo}:${tag} install ${verbosity} | tee /tmp/icp-install-log.txt
+
+exit $?

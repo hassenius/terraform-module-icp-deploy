527a5f38ba225cde28030ada436af85a6c8bee96
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2019-01-07 17:40:19 +0000

    refactor image load for multi-arch

--- scripts/boot-master/load-image.sh
+++ scripts/boot-master/load-image.sh
@@ -3,20 +3,49 @@ LOGFILE=/tmp/loadimage.log
 exec 3>&1
 exec > >(tee -a ${LOGFILE} >/dev/null) 2> >(tee -a ${LOGFILE} >&3)
 
-while getopts ":v:l:" arg; do
-    case "${arg}" in
-      v)
-        image=${OPTARG}
-        ;;
-      l)
-        image_location=${OPTARG}
-        ;;
-    esac
+echo "Got the parameters $@"
+# Defaults
+source /tmp/icp-bootmaster-scripts/functions.sh
+sourcedir=/opt/ibm/cluster/images
+declare -a locations
+
+# Parse options
+while getopts ":l:i:s:u:p:" opt; do
+  case $opt in
+    l)
+      locations+=( ${OPTARG} )
+      ;;
+    u)
+      username=${OPTARG}
+      ;;
+    p)
+      password=${OPTARG}
+      ;;
+    i)
+      image=${OPTARG}
+      ;;
+    s)
+      echo "Will overwrite default sourcedir to ${OPTARG}"
+      sourcedir=${OPTARG}
+      ;;
+    \?)
+      echo "Invalid option : -$OPTARG in commmand $0 $*" >&2
+      exit 1
+      ;;
+    :)
+      echo "Missing option argument for -$OPTARG in command $0 $*" >&2
+      exit 1
+      ;;
+  esac
 done
 
-sourcedir=/opt/ibm/cluster/images
+# Figure out the version
+# This will populate $org $repo and $tag
+parse_icpversion ${image}
+echo "registry=${registry:-not specified} org=$org repo=$repo tag=$tag"
 
-source /tmp/icp-bootmaster-scripts/functions.sh
+# Make sure sourcedir exists, in case we need to donwload some archives
+mkdir -p ${sourcedir}
 
 if [[ ! -z ${image} ]]; then
   # Figure out the version
@@ -25,45 +54,53 @@ if [[ ! -z ${image} ]]; then
   echo "registry=${registry:-not specified} org=$org repo=$repo tag=$tag"
 fi
 
-if [[ "${image_location}" != "false" ]]
-then
-  # Decide which protocol to use
-  if [[ "${image_location:0:3}" == "nfs" ]]
-  then
+# Allow downloading multiple tarballs,
+# which is required in multi-arch deployments
+for image_location in ${locations[@]} ; do
+
+  # Detect which protocol to use
+  if [[ "${image_location:0:4}" == "http" ]]; then
+    # Extract filename from URL if possible
+    if [[ "${image_location: -2}" == "gz" ]]; then
+      # Assume a sensible filename can be extracted from URL
+      filename=$(basename ${image_location})
+    else
+      # TODO We might be able to use some magic to extract actual filename.
+      # For now, hard-code for x86
+      echo "Not able to determine filename from URL ${image_location}" >&2
+      filename="ibm-cloud-private-x86_64${tag}.tar.gz"
+      echo "Set it to ${filename}" >&2
+
+    fi
+
+    # Download the file using auth if provided
+    echo "Downloading ${image_location}" >&2
+    wget --continue ${username:+--user} ${username} ${password:+--password} ${password} \
+     -O ${sourcedir}/${filename} "${image_location}"
+
+    if [[ $? -gt 0 ]]; then
+      echo "Error downloading ${image_location}" >&2
+      exit 1
+    fi
+
+    # Set the image file name if we're on the same platform
+    if [[ ${filename} =~ .*$(uname -m).* ]]; then
+      echo "Setting image_file to ${sourcedir}/${filename}"
+      image_file="${sourcedir}/${filename}"
+    fi
+  else
+    # Assume NFS since this is the only other supported protocol
     # Separate out the filename and path
-    nfs_mount=$(dirname ${image_location:4})
+    nfs_mount=$(dirname ${image_location})
     image_file="${sourcedir}/$(basename ${image_location})"
     mkdir -p ${sourcedir}
     # Mount
     sudo mount.nfs $nfs_mount $sourcedir
-  elif [[ "${image_location:0:4}" == "http" ]]
-  then
-    # Figure out what we should name the file
-    filename="ibm-cloud-private-x86_64-${tag%-ee}.tar.gz"
-    mkdir -p ${sourcedir}
-    # Figure out if we need auth
-    if [[ ${image_location} =~ http:.*:.*@http.* ]]
-    then
-      # Save the auth section and extract username password
-      auth=${image_location%%@http*}
-      userpass=${auth#http:}
-      htuser=${userpass%%:*}
-      htpass=${userpass#*:}
 
-      image_url=${image_location##*@}
-    else
-      image_url=${image_location#http:}
-    fi
-
-    # Download the file using auth if provided
-    wget --continue ${htuser:+--user} ${htuser} ${htpass:+--password} ${htpass} \
-     -O ${sourcedir}/${filename} "${image_url}"
-
-    # Set the image file name
-    image_file="${sourcedir}/${filename}"
   fi
-fi
+done
 
+# Load the offline tarball to get the inception image
 if [[ -s "$image_file" ]]
 then
   echo "Loading image file ${image_file}. This can take a very long time" >&2
